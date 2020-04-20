84b0838a21a3707cf601f67c727a9b9e0fca44de
Ziagl <office@ziegelwanger-edv.at>
2020-04-20 11:54:15 +0200

    updated input for smooth scrolling

--- 12_input/Main.asm
+++ 12_input/Main.asm
@@ -29,10 +29,16 @@ ddrb =  $dc03            ; data direction register B
         sta $d000
         sta $d001
 
-LOOP    jsr check_w
-        ;jsr check_a
+LOOP    
+        ; wait for scanline
+        lda $d012
+        cmp $d012
+        bne *-3   ;check *until* we're at the target raster line
+        ; input check
+        jsr check_w
+        jsr check_a
         jsr check_s
-        ;jsr check_d
+        jsr check_d
         jmp LOOP
 
 ; movement routines
@@ -44,6 +50,14 @@ check_w
         beq go_up
         rts
 
+check_a
+        lda #%11111101  ; select row 2
+        sta pra 
+        lda prb         ; load column information
+        and #%00000100  ; test 'a' key  
+        beq go_left
+        rts
+
 check_s
         lda #%11111101  ; select row 2
         sta pra 
@@ -52,19 +66,43 @@ check_s
         beq go_down
         rts
 
+check_d
+        lda #%11111011  ; select row 3
+        sta pra 
+        lda prb         ; load column information
+        and #%00000100  ; test 'd' key  
+        beq go_right
+        rts
+
 go_up                 
         lda $d001
-        cmp #$ff        ; check Y-coord whether we are too high
+        cmp #$00        ; check Y-coord whether we are too high
         beq @skip       ; if top of screen reached, skip
-        inc $d001       ; increase y-coord for sprite 1
+        dec $d001       ; increase y-coord for sprite 1
 @skip
         rts
 
 go_down
         lda $d001
+        cmp #$ff
+        beq @skip
+        inc $d001
+@skip
+        rts
+
+go_right                
+        lda $d000
+        cmp #$ff   
+        beq @skip
+        inc $d000
+@skip
+        rts
+
+go_left
+        lda $d000
         cmp #$00
         beq @skip
-        dec $d001
+        dec $d000
 @skip
         rts
 
